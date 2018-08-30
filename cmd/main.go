6676d1f4c95164a8609697e86494db3ebae55a61
zhangke <zhangke>
2018-08-30 17:13:36 +0800

    update

--- cmd/main.go
+++ cmd/main.go
@@ -3,33 +3,40 @@ package main
 import (
 	"log"
 
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"github.com/godaner/go-route/route"
+	"github.com/godaner/go-util"
 	"github.com/godaner/wxrobot"
 	"github.com/larspensjo/config"
-	"fmt"
-	"wxautoreplyrobot"
-	"github.com/godaner/go-util"
+	"github.com/skip2/go-qrcode"
+	"image/jpeg"
 	"os"
-	"github.com/godaner/go-route/route"
-	"wxautoreplyrobot/handler"
 	"time"
+	"wxautoreplyrobot"
+	"wxautoreplyrobot/handler"
+	"strings"
+	"net/smtp"
 )
 
 const (
-	TIME_LAYOUT="2006-01-02 15:04:05"
+	TIME_LAYOUT = "2006-01-02 15:04:05"
 )
-func init(){
-	//check
+
+func init() {
+	//check cfg
 	if !go_util.FileExists(wxautoreplyrobot.TextReplyPath) {
 		os.Create(wxautoreplyrobot.TextReplyPath)
 	}
 	c, err := config.ReadDefault(wxautoreplyrobot.TextReplyPath)
-	if err!=nil {
-		log.Println("init err : ",err)
+	if err != nil {
+		log.Println("init err : ", err)
 	}
-	if !c.HasSection("msg"){
-		c.AddOption("msg","hello","i am wxautoreplyrobot! birthday is : "+time.Now().Format(TIME_LAYOUT))
+	if !c.HasSection("msg") {
+		c.AddOption("msg", "hello", "i am wxautoreplyrobot! birthday is : "+time.Now().Format(TIME_LAYOUT))
 	}
-	c.WriteFile(wxautoreplyrobot.TextReplyPath,os.ModeDevice,"")
+	c.WriteFile(wxautoreplyrobot.TextReplyPath, os.ModeDevice, "")
 }
 
 func main() {
@@ -44,44 +51,102 @@ func main() {
 	go route.Start(wxautoreplyrobot.Addr)
 
 	//// wxrobot ////
-	for{
-		for {//show login qr and get contact list
-			err0 := wxrobot.Init(&wxrobot.MessageHandler{
-				TextHandler: textHandler,
+	// forever retry show login qr
+	for {
+		for {
+			wxrobot.SetClientHandler(&wxrobot.Handler{
+				TextHandler:   textHandler,
+				ShowQRHandler: showQRHandler,
 			})
-			if err0 != nil {
-				log.Printf(err0.Error())
+			err := wxrobot.StartClient() //will be block
+			if err != nil {
+				log.Printf("wxrobot client err , err is : %s , time is %s !", err.Error(), time.Now().Format(TIME_LAYOUT))
 				continue
 			}
 			break
-		}
-
-		//have login success
-		err:=wxrobot.Listening()
-
-		if err!=nil{
-			log.Printf("wxrobot litening err , err is : %s , time is %s !",err.Error(),time.Now().Format(TIME_LAYOUT))
 		}
 	}
 
 }
 
-
-func textHandler(msg *wxrobot.Message){
+func textHandler(msg *wxrobot.Message) {
 	defer func() {
-		if err := recover();err != nil {
+		if err := recover(); err != nil {
 			fmt.Println(err)
 		}
 	}()
 	c, _ := config.ReadDefault(wxautoreplyrobot.TextReplyPath)
 	//reply
-	reply,err:=c.String("msg", msg.Content)
-	if err!=nil {
+	reply, err := c.String("msg", msg.Content)
+	if err != nil {
 		//log.Println("textHandler : get reply is err ! err is : ",err)
 		return
 	}
-	if reply==""{
+	if reply == "" {
 		return
 	}
-	wxrobot.SendMsg(msg.FromUserName,reply)
+	wxrobot.SendMsg(msg.FromUserName, reply)
+}
+func showQRHandler(qrStrP *string) {
+	if wxautoreplyrobot.Email == ""{
+		return
+	}
+	////qr page////
+	base64qr, _ := generateQRBase64(*qrStrP, 120)
+	content := `
+	<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Reply List</title>
+</head>
+<body>
+	Please scan this qr to login wx:
+	<img src="` + base64qr + `"/>
+</body>
+</html>
+`
+	////qr email////
+
+	err := SendToMail("godanermail@gmail.com", "ZK19951217.", "smtp.gmail.com:465", wxautoreplyrobot.Email, "wxrobot login", content, "html")
+
+
+	if  err != nil {
+		log.Printf("Send emial to %s err , err is : %s ! ",wxautoreplyrobot.Email,err.Error())
+	}else {
+		log.Printf("Send emial to %s success ! ",wxautoreplyrobot.Email)
+	}
+}
+func generateQRBase64(qrCode string, size int) (string, error) {
+	if size <= 0 {
+		size = 250
+	}
+	q, err := qrcode.New(qrCode, qrcode.Medium)
+	if err != nil {
+		return "", err
+	}
+	image := q.Image(size)
+	if err != nil {
+		return "", err
+	}
+	//image to base 64
+	emptyBuff := bytes.NewBuffer(nil)
+	jpeg.Encode(emptyBuff, image, nil)
+
+	return "data:image/jpeg;base64," + base64.StdEncoding.EncodeToString(emptyBuff.Bytes()), nil
+}
+func SendToMail(user, password, host, to, subject, body, mailtype string) error {
+	hp := strings.Split(host, ":")
+	auth := smtp.PlainAuth("", user, password, hp[0])
+	var content_type string
+	if mailtype == "html" {
+		content_type = "Content-Type: text/" + mailtype + "; charset=UTF-8"
+	} else {
+		content_type = "Content-Type: text/plain" + "; charset=UTF-8"
+	}
+
+	msg := []byte("To: " + to + "\r\nFrom: " + user + ">\r\nSubject: " + "\r\n" + content_type + "\r\n\r\n" + body)
+	send_to := strings.Split(to, ";")
+	err := smtp.SendMail(host, auth, user, send_to, msg)
+	return err
 }
--- config.go
+++ config.go
@@ -4,12 +4,15 @@ import "flag"
 
 var TextReplyPath string
 var Addr string
+var Email string
 func init(){
 	textReplyPath :=flag.String("textReplyPath","","")
 	addr :=flag.String("addr",":80","")
+	email :=flag.String("email","1138829222@qq.com","")
 
 	flag.Parse()
 
 	TextReplyPath=*textReplyPath
 	Addr=*addr
+	Email=*email
 }
